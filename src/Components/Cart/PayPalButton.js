fdb8eb7f5344b4dd3a43e7997387585c7f90b8b2
ajadi yusuf <yusufajadi93@gmail.com>
2020-03-31 13:16:12 +0100

    deleted the sand box value

--- src/Components/Cart/PayPalButton.js
+++ src/Components/Cart/PayPalButton.js
@@ -30,7 +30,7 @@ export default class MyApp extends React.Component {
         // Document on Paypal's currency code: https://developer.paypal.com/docs/classic/api/currency_codes/
  
         const client = {
-            sandbox: 'AYwQL5IKaVgoI2z-x66s6BCVtQHzD_RNwjwFaQzXF8KJ3CJLeDu4eOVUAqCJRMCRPkYOxlRi9p5qK7di' ,
+            sandbox: '',
             production: 'YOUR-PRODUCTION-APP-ID',
         }
         // In order to get production's app-ID, you will have to send your app to Paypal for approval first

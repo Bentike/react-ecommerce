31977d6d700626e6cf818d98fdb3488f3e4e8883
ajadi yusuf <yusufajadi93@gmail.com>
2020-09-15 09:15:23 +0100

    Updates

---
+++ precache-manifest.17533cbfad17d775bf3b3d6b2be5ad52.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96fe6845d959eb29fadd004a679cbf53",
+    "url": "/Bentike/react-ecommerce/index.html"
+  },
+  {
+    "revision": "d48e76c4843777014cd2",
+    "url": "/Bentike/react-ecommerce/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "b7f0b7c14f387af4b491",
+    "url": "/Bentike/react-ecommerce/static/css/main.82b0f704.chunk.css"
+  },
+  {
+    "revision": "d48e76c4843777014cd2",
+    "url": "/Bentike/react-ecommerce/static/js/2.84e88514.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/Bentike/react-ecommerce/static/js/2.84e88514.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7f0b7c14f387af4b491",
+    "url": "/Bentike/react-ecommerce/static/js/main.c3fd2f9e.chunk.js"
+  },
+  {
+    "revision": "cfdb1c5241a870f5d3fe",
+    "url": "/Bentike/react-ecommerce/static/js/runtime-main.78ffb895.js"
+  }
+]);
